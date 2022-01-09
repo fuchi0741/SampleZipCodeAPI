b2ef1667aa05b9cb46964dcb949f4f4007132e60
Kazuma Fuchi <fuchi.kazuma@gmail.com>
2022-01-09 10:05:47 +0900

    第17回_API超基礎_HTTPリクエストメソッド実装

--- SampleZipCodeAPI/Model/ZipCodeManager.swift
+++ SampleZipCodeAPI/Model/ZipCodeManager.swift
@@ -10,13 +10,19 @@ import Foundation
 struct ZipCodeManager {
     static func fetchAddress(zipCode: String) {
         let baseURL = "https://zipcloud.ibsnet.co.jp/api/search"
-        
+         
         guard var urlComponents = URLComponents(string: baseURL) else { return }
         
         urlComponents.queryItems = [
             URLQueryItem(name: "zipcode", value: zipCode),
         ]
+                
+        guard let requestURL = urlComponents.url else { return }
         
-        print("😆", urlComponents)
+        let task = URLSession.shared.dataTask(with: requestURL) { (data, response, error) in
+            print("レスポンスが返ってきてきてから実行")
+        }
+        
+        task.resume()
     }
 }
