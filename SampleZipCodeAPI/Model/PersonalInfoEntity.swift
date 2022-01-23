5e269073421eb62846302d589c126f71ae5cfab0
KAZUMA FUCHI <fuchi.kazuma@gmail.com>
2022-01-23 15:31:37 +0900

    第38回_API超基礎_個人情報Entity実装

---
+++ SampleZipCodeAPI/Model/PersonalInfoEntity.swift
@@ -0,0 +1,14 @@
+//
+//  PersonalInfoEntity.swift
+//  SampleZipCodeAPI
+//
+//  Created by 渕一真 on 2022/01/23.
+//
+
+import Foundation
+
+struct PersonalInfoEntity {
+    let name: String
+    let phoneNum: String
+    let address: String
+}
