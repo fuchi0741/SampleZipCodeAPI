8c8ba2550929c245c2db2384cee942e80a312708
KAZUMA FUCHI <fuchi.kazuma@gmail.com>
2022-01-30 17:09:50 +0900

    第49回_API超基礎_Realm処理をModel層にリファクタリング概要

---
+++ SampleZipCodeAPI/Model/RealmManager.swift
@@ -0,0 +1,23 @@
+//
+//  RealmManager.swift
+//  SampleZipCodeAPI
+//
+//  Created by 渕一真 on 2022/01/30.
+//
+
+import Foundation
+import RealmSwift
+
+struct RealmManager {
+    static func savePersonalInfo() {
+        /// Realmにデータを保存する
+    }
+    
+    static func getSavedPersonalInfoCount() {
+        /// Realmに保存したデータの数を取得するメソッド
+    }
+    
+    static func getSavedPersonalInfoEntity() {
+        /// Realmに保存したデータを取得する
+    }
+}
